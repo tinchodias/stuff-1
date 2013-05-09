236e60504ce654c7b6be64aec0d6bb6b879d86a7
Max Leske <maxleske@gmail.com>
2013-05-09 18:42:11 +0200

    updated script with more options

--- fuel/fuel_test_automator.sh
+++ fuel/fuel_test_automator.sh
@@ -1,17 +1,25 @@
-if [ -z "$1" ]; then
-	echo "You did not supply the mode argument: 1 to run all tests, 2 to serialize / materialize all objects to / from all images"
-	exit 1
-elif [ "$1" -ne 1 ] && [ "$1" -ne 2 ]; then
-	echo "Mode was $1 but should have been 1 or 2."
-	exit 1
-fi
+#!/bin/bash
 
-MODE="$1"
+# usage
+# ============
+#
+# fuel_test_automator [-ms]:
+#
+#
+# options
+# ============
+#
+# -m serialize / materialize all objects to / from all images (tests will not be run)
+# -s load latest stable (instead of bleeding edge)
+
+SERIALIZE_MATERIALIZE=0
+LOAD_STABLE=0
 VM_PATH="/devel/VM"
 IMAGE_PATH="/devel/fuel"
 COG_VM="$VM_PATH/CogVM.app/Contents/MacOS/CogVM"
 STACK_VM="$VM_PATH/Squeak 4.2.5beta1U.app/Contents/MacOS/Squeak VM Opt"
-LOADER_SCRIPT="$IMAGE_PATH/fuel_load_packages.st"
+LOADER_SCRIPT_STABLE="$IMAGE_PATH/fuel_load_packages_stable.st"
+LOADER_SCRIPT_BLEEDING_EDGE="$IMAGE_PATH/fuel_load_packages_bleeding_edge.st"
 TEST_RUNNER_SCRIPT="$IMAGE_PATH/fuel_run_all_tests.st"
 SERIALIZATION_SCRIPT="$IMAGE_PATH/fuel_serialize_all_objects.st"
 MATERIALIZATION_SCRIPT="$IMAGE_PATH/fuel_materialize_all_objects.st"
@@ -22,23 +30,88 @@ PHARO_IMAGES=("Pharo111" "Pharo112" "Pharo12" "Pharo13" "Pharo14" "Pharo20")
 SQUEAK_IMAGES_STACK_VM=("Squeak41")
 SQUEAK_IMAGES_COG_VM=("Squeak42" "Squeak43" "Squeak44")
 
+
+while getopts ":m:s:?" OPT ; do
+	case "$OPT" in
+
+		# serialize / materialize objects
+		m)	SERIALIZE_MATERIALIZE=1
+		;;
+		
+		# load latest stable
+		s)	LOAD_STABLE=1
+		;;
+		
+		# show help
+		h)	display_help
+		;;
+		
+		# show help
+		\?)	display_help
+			exit 1
+		;;
+
+	esac
+done
+
+if [ $LOAD_STABLE = 1 ]; then
+	echo "loading stable"
+else
+	echo "loading bleeding edge"
+fi
+
+if [ $SERIALIZE_MATERIALIZE = 1 ]; then
+	echo "serializing / materializing all objects"
+else
+	echo "running all tests"
+fi
+
+#pharo cog
+for image in ${PHARO_IMAGES[@]}; do
+	prepare_final_script ${image}
+	echo "running COG_VM $IMAGE_PATH/${image}/${image}.image $FINAL_SCRIPT"
+	exec "$COG_VM" "$IMAGE_PATH/${image}/${image}.image" "$FINAL_SCRIPT" &
+done
+
+#squeak stack
+for image in ${SQUEAK_IMAGES_STACK_VM[@]}; do
+	prepare_final_script ${image}
+	echo "running $STACK_VM $IMAGE_PATH/${image}/${image}.image $FINAL_SCRIPT"
+	exec "$STACK_VM" "$IMAGE_PATH/${image}/${image}.image" "$FINAL_SCRIPT" &
+done
+
+#squeak cog
+for image in ${SQUEAK_IMAGES_COG_VM[@]}; do
+	prepare_final_script ${image}
+	echo "running $COG_VM $IMAGE_PATH/${image}/${image}.image $FINAL_SCRIPT"
+	exec "$COG_VM" "$IMAGE_PATH/${image}/${image}.image" "$FINAL_SCRIPT" &
+done
+
+exit 0
+
+# help function
+function display_help() {
+	echo "$(basename $0) [-ms]"
+	echo " -m serialize / materialize all objects to / from all images (tests will not be run)"
+	echo " -s load latest stable (instead of bleeding edge)"
+}
+
 function prepare_final_script(){
 	IMAGE_NAME=$1
 	FINAL_SCRIPT="${FINAL_SCRIPT_BASE}_${IMAGE_NAME}.st"
-	echo $FINAL_SCRIPT
-	if [ $MODE = 1 ]; then
-		echo "running all tests"
-		
-		cat "$LOADER_SCRIPT" > "$FINAL_SCRIPT"
-		cat "$TEST_RUNNER_SCRIPT" >> "$FINAL_SCRIPT"
+	if [ $LOAD_STABLE = 1 ]; then
+		cat "$LOADER_SCRIPT_STABLE" > "$FINAL_SCRIPT"
 	else
-		echo "serializing / materializing all objects"
-		
-		cat "$LOADER_SCRIPT" > "$FINAL_SCRIPT"
-		echo -e "\n" >> "$FINAL_SCRIPT"
+		cat "$LOADER_SCRIPT_BLEEDING_EDGE" > "$FINAL_SCRIPT"
+	fi
+	
+	if [ $SERIALIZE_MATERIALIZE = 0 ]; then
+		cat "$TEST_RUNNER_SCRIPT" >> "$FINAL_SCRIPT"
+	else		
+		echo "\n" >> "$FINAL_SCRIPT\c"
 		
 		echo "Smalltalk at: #FuelFormatTestScriptsPath put: '$IMAGE_PATH'." >> "$FINAL_SCRIPT"
-		echo -e "\n" >> "$FINAL_SCRIPT"
+		echo "\n" >> "$FINAL_SCRIPT\c"
 		
 		echo "Smalltalk at: #FuelFormatTestImageNames put: #(" >> "$FINAL_SCRIPT"
 		for image in ${PHARO_IMAGES[@]}; do
@@ -50,37 +123,14 @@ function prepare_final_script(){
 		for image in ${SQUEAK_IMAGES_COG_VM[@]}; do
 			echo "'${image}' " >> "$FINAL_SCRIPT"
 		done
-		echo -e ").\n" >> "$FINAL_SCRIPT"
+		echo ").\n" >> "$FINAL_SCRIPT\c"
 		
 		echo "Smalltalk at: #FuelFormatTestFilename put: '$IMAGE_NAME.fuel'." >> "$FINAL_SCRIPT"
-		echo -e "\n" >> "$FINAL_SCRIPT"
+		echo "\n" >> "$FINAL_SCRIPT\c"
 		
 		cat "$SERIALIZATION_SCRIPT" >> "$FINAL_SCRIPT"
-		echo -e "\n" >> "$FINAL_SCRIPT"
+		echo "\n" >> "$FINAL_SCRIPT\c"
 		
 		cat "$MATERIALIZATION_SCRIPT" >> "$FINAL_SCRIPT"
 	fi
 }
-
-#pharo cog
-# for image in ${PHARO_IMAGES[@]}; do
-# 	prepare_final_script ${image}
-# 	echo "running COG_VM $IMAGE_PATH/${image}/${image}.image $FINAL_SCRIPT"
-# 	exec "$COG_VM" "$IMAGE_PATH/${image}/${image}.image" "$FINAL_SCRIPT" &
-# done
-
-#squeak stack
-for image in ${SQUEAK_IMAGES_STACK_VM[@]}; do
-	prepare_final_script ${image}
-	echo "running $STACK_VM $IMAGE_PATH/${image}/${image}.image $FINAL_SCRIPT"
-	exec "$STACK_VM" "$IMAGE_PATH/${image}/${image}.image" "$FINAL_SCRIPT" &
-done
-# 
-# #squeak cog
-# for image in ${SQUEAK_IMAGES_COG_VM[@]}; do
-# 	prepare_final_script ${image}
-# 	echo "running $COG_VM $IMAGE_PATH/${image}/${image}.image $FINAL_SCRIPT"
-# 	exec "$COG_VM" "$IMAGE_PATH/${image}/${image}.image" "$FINAL_SCRIPT" &
-# done
-# 
-# exit 0
