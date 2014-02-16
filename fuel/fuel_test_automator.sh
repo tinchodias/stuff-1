79cf3e433352d89df8b5846258ba245c73e835b4
Max Leske <maxleske@gmail.com>
2014-02-16 19:12:26 +0100

    automatically load latest configuration; added Pharo30, Squeak45 and Pharo122

--- fuel/fuel_test_automator.sh
+++ fuel/fuel_test_automator.sh
@@ -17,7 +17,9 @@ LOAD_STABLE=0
 VM_PATH="/devel/VM"
 IMAGE_PATH="/devel/fuel"
 COG_VM="$VM_PATH/CogVM.app/Contents/MacOS/CogVM"
+PHARO_VM="$VM_PATH/Pharo.app/Contents/MacOS/Pharo"
 STACK_VM="$VM_PATH/Squeak 4.2.5beta1U.app/Contents/MacOS/Squeak VM Opt"
+PREPARATION_SCRIPT="$IMAGE_PATH/fuel_fix_installer_for_squeak.st"
 LOADER_SCRIPT_STABLE="$IMAGE_PATH/fuel_load_packages_stable.st"
 LOADER_SCRIPT_BLEEDING_EDGE="$IMAGE_PATH/fuel_load_packages_bleeding_edge.st"
 TEST_RUNNER_SCRIPT="$IMAGE_PATH/fuel_run_all_tests.st"
@@ -26,9 +28,10 @@ MATERIALIZATION_SCRIPT="$IMAGE_PATH/fuel_materialize_all_objects.st"
 FINAL_SCRIPT_BASE="$IMAGE_PATH/run_it"
 FINAL_SCRIPT=""
 
-PHARO_IMAGES=("Pharo111" "Pharo112" "Pharo12" "Pharo13" "Pharo14" "Pharo20")
+PHARO_IMAGES_COG_VM=("Pharo111" "Pharo112" "Pharo12" "Pharo122" "Pharo13" "Pharo14" "Pharo20")
+PHARO_IMAGES_PHARO_VM=("Pharo30")
 SQUEAK_IMAGES_STACK_VM=("Squeak41")
-SQUEAK_IMAGES_COG_VM=("Squeak42" "Squeak43" "Squeak44")
+SQUEAK_IMAGES_COG_VM=("Squeak42" "Squeak43" "Squeak44" "Squeak45")
 
 # help function
 function display_help() {
@@ -40,10 +43,11 @@ function display_help() {
 function prepare_final_script(){
 	IMAGE_NAME=$1
 	FINAL_SCRIPT="${FINAL_SCRIPT_BASE}_${IMAGE_NAME}.st"
+	cat "$PREPARATION_SCRIPT" > "$FINAL_SCRIPT"
 	if [ $LOAD_STABLE = 1 ]; then
-		cat "$LOADER_SCRIPT_STABLE" > "$FINAL_SCRIPT"
+		cat "$LOADER_SCRIPT_STABLE" >> "$FINAL_SCRIPT"
 	else
-		cat "$LOADER_SCRIPT_BLEEDING_EDGE" > "$FINAL_SCRIPT"
+		cat "$LOADER_SCRIPT_BLEEDING_EDGE" >> "$FINAL_SCRIPT"
 	fi
 	
 	if [ $SERIALIZE_MATERIALIZE = 0 ]; then
@@ -55,7 +59,10 @@ function prepare_final_script(){
 		echo "\n" >> "$FINAL_SCRIPT\c"
 		
 		echo "Smalltalk at: #FuelFormatTestImageNames put: #(" >> "$FINAL_SCRIPT"
-		for image in ${PHARO_IMAGES[@]}; do
+		for image in ${PHARO_IMAGES_COG_VM[@]}; do
+			echo "'${image}' " >> "$FINAL_SCRIPT"
+		done
+		for image in ${PHARO_IMAGES_PHARO_VM[@]}; do
 			echo "'${image}' " >> "$FINAL_SCRIPT"
 		done
 		for image in ${SQUEAK_IMAGES_STACK_VM[@]}; do
@@ -113,10 +120,17 @@ else
 fi
 
 #pharo cog
-for image in ${PHARO_IMAGES[@]}; do
+for image in ${PHARO_IMAGES_COG_VM[@]}; do
 	prepare_final_script ${image}
-	echo "running COG_VM $IMAGE_PATH/${image}/${image}.image $FINAL_SCRIPT"
+	echo "running $COG_VM $IMAGE_PATH/${image}/${image}.image $FINAL_SCRIPT"
 	exec "$COG_VM" "$IMAGE_PATH/${image}/${image}.image" "$FINAL_SCRIPT" &
+done
+
+#pharo vm
+for image in ${PHARO_IMAGES_PHARO_VM[@]}; do
+	prepare_final_script ${image}
+	echo "running $PHARO_VM $IMAGE_PATH/${image}/${image}.image $FINAL_SCRIPT"
+	exec "$PHARO_VM" "$IMAGE_PATH/${image}/${image}.image" "$FINAL_SCRIPT" &
 done
 
 #squeak stack
@@ -134,5 +148,3 @@ for image in ${SQUEAK_IMAGES_COG_VM[@]}; do
 done
 
 exit 0
-
-
