392e2a3ad354e762368dbb38bdff04056d0cb97b
Max Leske <maxleske@gmail.com>
2014-02-17 13:35:59 +0100

    fixing ftp...

--- fuel/jenkins.sh
+++ fuel/jenkins.sh
@@ -4,12 +4,14 @@ export JOB_NAME=`dirname $JOB_NAME`
 REPO=http://smalltalkhub.com/mc/Pharo/Fuel/main
 
 if [[ "$SYSTEM" == "pharo"* ]]; then
-    # build and test pharo images
+    # build and test pharo images    
     if [ "$SYSTEM" == "pharo20" ]; then
         IMAGE_VERSION="20"
     else
         IMAGE_VERSION="30"
     fi
+    
+    echo "downloading image and vm"
     wget --quiet -O - get.pharo.org/$IMAGE_VERSION+vm | bash
 
     #was used to debug build failures (max)
@@ -18,18 +20,21 @@ if [[ "$SYSTEM" == "pharo"* ]]; then
     #unzip -j 30413.zip
     #mv Pharo-30413.image Pharo.image
     #mv Pharo-30413.changes Pharo.changes
-
     ./pharo Pharo.image save $JOB_NAME --delete-old
     ./pharo $JOB_NAME.image --version > version.txt
 
     #was used to debug build failures (max)
     #./pharo $JOB_NAME eval -e "Author useAuthor: 'foo' during: [OpalCompiler class compile: 'defaultOptions ^ #(+ optionInlineIf + optionInlineIfNil + optionInlineAndOr + optionInlineWhile + optionInlineToDo + optionInlineCase - optionInlineTimesRepeat + optionIlineNone + optionLongIvarAccessBytecodes  + optionOptimizeIR )'.OpalCompiler recompileAll].Smalltalk snapshot: true andQuit: true."
+    echo "building image"
     ./pharo $JOB_NAME.image config $REPO ConfigurationOf$JOB_NAME --install=$VERSION --group=$GROUP
+    echo "running tests"
     ./pharo $JOB_NAME.image test --junit-xml-output "$JOB_NAME.*"
 else
     # build and test squeak images
+    echo "downloading sources"
     wget --quiet http://ftp.squeak.org/current_stable/SqueakV41.sources.zip
     unzip -j SqueakV41.sources.zip
+    echo "downloading vm"
     wget --quiet http://ftp.squeak.org/current_stable/unix-linux/coglinux.tgz
     tar -xzf coglinux.tgz
     
@@ -42,27 +47,33 @@ else
     fi
     IMAGE_DIRECTORY_URL="${IMAGE_FTP_HOST}/${IMAGE_DIRECTORY}/"
     
+    echo "listing builds on ftp server"
     # -i: no interactive prompt, -n don't ask for login, -p enforces passive mode
-    ftp -ipn "$IMAGE_FTP_HOST" << EOF
-    user anonymous pharo-fuel@lists.gforge.inria.fr
-    cd "$IMAGE_DIRECTORY"
-    ls . ftp_ls.log
-    quit
-    EOF
+    ftp -ipn "$IMAGE_FTP_HOST" <<EOF
+user anonymous pharo-fuel@lists.gforge.inria.fr
+cd "$IMAGE_DIRECTORY"
+ls . ftp_ls.log
+quit
+EOF
+    
+    echo "found the following files on the ftp server: "
+    cat ftp_ls.log
 
     # parse the file name of the most recent build from the ftp ls output
-    IMAGE_FILE_NAME=cat ftp_ls.log | grep -E "Squeak[0-9]\.[0-9]\-[0-9]+\.zip" | sed -E 's/.*(Squeak.*\.zip)/\1/p' | sort | tail -1
+    IMAGE_FILE_NAME=`cat ftp_ls.log | grep -E "Squeak[0-9]\.[0-9]\-[0-9]+\.zip" | sed -E 's/.*(Squeak.*\.zip)/\1/p' | sort | tail -1`
     # parse the file extension from the filename (not necessary at the moment)
-    IMAGE_FILE_EXTENSION=echo $IMAGE_FILE_NAME | sed -E 's/.*\.([a-zA-Z0-9]+)/\1/p'
+    #IMAGE_FILE_EXTENSION=`echo $IMAGE_FILE_NAME | sed -E 's/.*\.([a-zA-Z0-9]+)/\1/p'`
     rm -f ftp_ls.log
 
-    wget --quiet "${IMAGE_DIRECTORY_URL}${IMAGE_FILE_NAME}.${IMAGE_FILE_EXTENSION}"
-    unzip -j "${IMAGE_FILE_NAME}.${IMAGE_FILE_EXTENSION}"
+    echo "downloading image: ${IMAGE_DIRECTORY_URL}${IMAGE_FILE_NAME}"
+    wget --quiet "${IMAGE_DIRECTORY_URL}${IMAGE_FILE_NAME}"
+    unzip -j "${IMAGE_FILE_NAME}"
     mv "${IMAGE_FILE_NAME}.image" "${JOB_NAME}.image"
     mv "${IMAGE_FILE_NAME}.changes" "${JOB_NAME}.changes"
     
     # generate the script to build image and run tests
-    cat << EOF > init_script.st
+    echo "generating init_script.st"
+    cat <<EOF > init_script.st
 "fix Installer for Smalltalkhub"
 [ (Smalltalk hasClassNamed: #InstallerInternetBased) ifTrue: [
 	Utilities setAuthorInitials: 'foo'.
@@ -89,9 +100,11 @@ EOF
     echo "] on: Error do: [ :ex | Smalltalk dumpException: ex ]. Smalltalk snapshot: true andQuit: true." >> init_script.st
     
     # build the image
+    echo "building image"
     ./coglinux/squeak -headless "${JOB_NAME}.image" "init_script.st"
 
-    cat << EOF > init_script.st
+    echo "generating init_script for tests"
+    cat <<EOF > init_script.st
 
 "load HudsonBuildTools and run tests"
 [ ((Smalltalk at: #Installer) monticello http: 'http://smalltalkhub.com/mc/Pharo/Fuel/') 
@@ -104,6 +117,7 @@ Smalltalk snapshot: false andQuit: true.
 EOF
     
     # run the tests
+    echo "running tests"
     ./coglinux/squeak -headless "${JOB_NAME}.image" "init_script.st"
     # remove those zip files we don't want to keep (jenkins will archive zip files)
     rm -f *.zip
